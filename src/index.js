1052ea079c79796c04dbd17381f005d5b7c011f7
Erika <pierreerika@MacBook-Pro-de-Erika.local>
2024-01-09 16:26:31 +0100

    config mongoose

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import express from "express"; // necessaire pour le back
 import dotenv from "dotenv"; // pour appeler notre fichier.env
+import mongoose from "mongoose";
 dotenv.config(); //pour appeler la configuration de l'environnement
 
 const app = express();
 const port = process.env.PORT;
 
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log(`📁DATABASE CONNECTED🐳`);
+}
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
-
-// main().catch((err) => console.log(err));
 
 app.listen(port, () =>
   console.log(`Server is listening on port : http://localhost:${port}`)

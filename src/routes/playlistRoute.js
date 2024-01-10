1639f54b0b701c8daf4c294e3e8e573aef5211dd
Erika <pierreerika@MacBook-Pro-de-Erika.local>
2024-01-10 16:43:57 +0100

    fixed bug

--- src/routes/playlistRoute.js
+++ src/routes/playlistRoute.js
@@ -12,6 +12,6 @@ playlistRouter.get("/all", getAllPlaylist);
 playlistRouter.post("/create", createPlaylist);
 playlistRouter.put("/update/:id", editPlaylist);
 playlistRouter.delete("/delete/:id", deletePlaylist);
-playlistRouter.delete("/:id_song/deletesong/:id_play", deleteSongPlaylist);
+playlistRouter.delete("/:id_song/delete-song/:id_play", deleteSongPlaylist);
 
 export { playlistRouter };
